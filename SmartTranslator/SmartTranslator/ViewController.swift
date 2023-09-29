cd2595676ffdc45652f28f773daa1e32a5034f4b
Ohlulu Hsih <z30262226@gmail.com>
2023-09-29 14:10:05 +0800

    chore: create project

---
+++ SmartTranslator/SmartTranslator/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  Copyright © 2023 Ohlulu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
